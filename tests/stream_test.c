538cb4eab30c051f1817159bdab434e371255c76
Victor Goya <victor.goya@af83.com>
2011-03-23 00:29:56 +0100

    add more tests on streams

--- tests/stream_test.c
+++ tests/stream_test.c
@@ -132,6 +132,8 @@ _read_handler (flm_Stream * stream, void * state, flm_Buffer * buffer)
 
     stream_in = (flm_Stream *) state;
     has_read = 1;
+    fail_unless (flm_BufferLength (buffer) == 1);
+    fail_unless (flm_BufferContent (buffer)[0] == 'a');
     flm_BufferRelease (buffer);
     flm_StreamClose (stream);
     flm_StreamClose (stream_in);
@@ -178,6 +180,161 @@ START_TEST(test_stream_read)
 }
 END_TEST
 
+static int nb_read = 0;
+static void
+_big_read_handler (flm_Stream * stream, void * state, flm_Buffer * buffer)
+{
+    flm_Stream * stream_in;
+    size_t i;
+
+    stream_in = (flm_Stream *) state;
+    nb_read += flm_BufferLength (buffer);
+    for (i = 0; i < flm_BufferLength (buffer); i++) {
+        fail_unless (flm_BufferContent (buffer)[i] == 'a');
+    }
+    flm_BufferRelease (buffer);
+
+    if (nb_read == 20000) {
+        flm_StreamClose (stream);
+        flm_StreamClose (stream_in);
+    }
+}
+
+START_TEST(test_stream_big_read)
+{
+    int baseFD;
+    int fds[2];
+    flm_Monitor * monitor;
+    flm_Stream * stream_in;
+    flm_Stream * stream_out;
+    size_t i;
+
+    setTestAlloc (0);
+
+    baseFD = getFDCount ();
+    fail_if (pipe (fds) == -1);
+
+    if ((monitor = flm_MonitorNew ()) == NULL) {
+        fail ("Monitor creatstreamn failed");
+    }
+    if ((stream_in = flm_StreamNew (monitor, fds[1], (void *)42)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    if ((stream_out = flm_StreamNew (monitor, fds[0], stream_in)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    flm_StreamOnRead (stream_out, _big_read_handler);
+    flm_StreamOnClose (stream_out, _close_handler);
+    flm_StreamOnClose (stream_in, _close_handler);
+
+    for (i = 0; i < 1000; i++) {
+        fail_if (write (fds[1], "aaaaaaaaaaaaaaaaaaaa", 20) == -1);
+    }
+
+    flm_StreamRelease (stream_in);
+    flm_StreamRelease (stream_out);
+
+    flm_MonitorWait (monitor);
+    flm_MonitorRelease (monitor);
+
+    fail_unless (nb_read == 20000);
+    fail_unless (nb_closed == 2);
+    fail_unless (getAllocSum () == 0);
+    fail_unless (getFDCount () == baseFD);
+}
+END_TEST
+
+START_TEST(test_stream_printf)
+{
+    int baseFD;
+    int fds[2];
+    flm_Monitor * monitor;
+    flm_Stream * stream_in;
+    flm_Stream * stream_out;
+    size_t i;
+
+    setTestAlloc (0);
+
+    baseFD = getFDCount ();
+    fail_if (pipe (fds) == -1);
+
+    if ((monitor = flm_MonitorNew ()) == NULL) {
+        fail ("Monitor creatstreamn failed");
+    }
+    if ((stream_in = flm_StreamNew (monitor, fds[1], (void *)42)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    if ((stream_out = flm_StreamNew (monitor, fds[0], stream_in)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    flm_StreamOnRead (stream_out, _big_read_handler);
+    flm_StreamOnClose (stream_out, _close_handler);
+    flm_StreamOnClose (stream_in, _close_handler);
+
+    for (i = 0; i < 1000; i++) {
+        flm_StreamPrintf (stream_in, "%saaaaaaaaaa", "aaaaaaaaaa");
+    }
+
+    flm_StreamRelease (stream_in);
+    flm_StreamRelease (stream_out);
+
+    flm_MonitorWait (monitor);
+    flm_MonitorRelease (monitor);
+
+    fail_unless (nb_read == 20000);
+    fail_unless (nb_closed == 2);
+    fail_unless (getAllocSum () == 0);
+    fail_unless (getFDCount () == baseFD);
+}
+END_TEST
+
+START_TEST(test_stream_push_buffer)
+{
+    int baseFD;
+    int fds[2];
+    flm_Monitor * monitor;
+    flm_Stream * stream_in;
+    flm_Stream * stream_out;
+    flm_Buffer * buffer;
+    size_t i;
+
+    setTestAlloc (0);
+
+    baseFD = getFDCount ();
+    fail_if (pipe (fds) == -1);
+
+    if ((monitor = flm_MonitorNew ()) == NULL) {
+        fail ("Monitor creatstreamn failed");
+    }
+    if ((stream_in = flm_StreamNew (monitor, fds[1], (void *)42)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    if ((stream_out = flm_StreamNew (monitor, fds[0], stream_in)) == NULL) {
+        fail ("Stream creatstreamn failed");
+    }
+    flm_StreamOnRead (stream_out, _big_read_handler);
+    flm_StreamOnClose (stream_out, _close_handler);
+    flm_StreamOnClose (stream_in, _close_handler);
+
+    for (i = 0; i < 1000; i++) {
+        buffer = flm_BufferNew ("aaaaaaaaaaaaaaaaaaaa", 20, NULL);
+        flm_StreamPushBuffer (stream_in, buffer, 0, 20);
+        flm_BufferRelease (buffer);
+    }
+
+    flm_StreamRelease (stream_in);
+    flm_StreamRelease (stream_out);
+
+    flm_MonitorWait (monitor);
+    flm_MonitorRelease (monitor);
+
+    fail_unless (nb_read == 20000);
+    fail_unless (nb_closed == 2);
+    fail_unless (getAllocSum () == 0);
+    fail_unless (getFDCount () == baseFD);
+}
+END_TEST
+
 Suite *
 stream_suite (void)
 {
@@ -191,6 +348,9 @@ stream_suite (void)
   tcase_add_test (tc_core, test_stream_shutdown);
   tcase_add_test (tc_core, test_stream_alloc_fail);
   tcase_add_test (tc_core, test_stream_read);
+  tcase_add_test (tc_core, test_stream_big_read);
+  tcase_add_test (tc_core, test_stream_printf);
+  tcase_add_test (tc_core, test_stream_push_buffer);
   suite_add_tcase (s, tc_core);
 
   return s;
